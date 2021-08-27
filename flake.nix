545c4e4c9fb3043395766196c84f8c2d8d94c9bd
leo60228 <leo@60228.dev>
2021-08-27 11:03:45 -0400

    upd8

--- flake.nix
+++ flake.nix
@@ -1,23 +1,34 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.pico-sdk = {
     flake = false;
-    url = "https://github.com/raspberrypi/pico-sdk.git?ref=1.1.2";
-    type = "git";
-    submodules = true;
+    url = "github:raspberrypi/pico-sdk/develop";
   };
 
-  outputs = { nixpkgs, flake-utils, pico-sdk, ... }:
+  inputs.tinyusb = {
+    flake = false;
+    url = "github:hathach/tinyusb/d49938d0f5052bce70e55c652b657c0a6a7e84fe";
+  };
+
+  outputs = { nixpkgs, flake-utils, pico-sdk, tinyusb, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages = {
           pico-sdk = with pkgs; stdenv.mkDerivation {
-            pname = "pico-sdk";
-            version = "1.1.2";
+            pname = "pico-sdk-unstable";
+            version =
+              let
+                date = pico-sdk.lastModifiedDate;
+                year = builtins.substring 0 4 date;
+                month = builtins.substring 4 2 date;
+                day = builtins.substring 6 2 date;
+                iso8601 = "${year}-${month}-${day}";
+              in
+                iso8601;
 
             src = pico-sdk;
 
@@ -27,6 +38,9 @@
             installPhase = ''
             mkdir -p $out/share
             cp -r . $out/share/pico-sdk
+
+            rm -rf $out/share/pico-sdk/lib/tinyusb
+            cp -r ${tinyusb} $out/share/pico-sdk/lib/tinyusb
             '';
 
             setupHook = writeText "setup-hook" ''
