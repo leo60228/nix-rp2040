ffdd6d610351bfdfd919b610fb68a911e77849c7
leo60228 <leo@60228.dev>
2021-08-28 13:26:07 -0400

    openocd

--- flake.nix
+++ flake.nix
@@ -12,23 +12,32 @@
     url = "github:hathach/tinyusb/d49938d0f5052bce70e55c652b657c0a6a7e84fe";
   };
 
-  outputs = { nixpkgs, flake-utils, pico-sdk, tinyusb, ... }:
+  inputs.openocd = {
+    flake = false;
+    type = "git";
+    url = "https://github.com/raspberrypi/openocd.git";
+    ref = "master+swd+rp2040+picoprobe";
+    submodules = true;
+    shallow = true;
+  };
+
+  outputs = { nixpkgs, flake-utils, pico-sdk, tinyusb, openocd, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+        makeIso8601 = date:
+          let
+            year = builtins.substring 0 4 date;
+            month = builtins.substring 4 2 date;
+            day = builtins.substring 6 2 date;
+            iso8601 = "${year}-${month}-${day}";
+          in
+            iso8601;
+      in rec {
         packages = {
           pico-sdk = with pkgs; stdenv.mkDerivation {
             pname = "pico-sdk-unstable";
-            version =
-              let
-                date = pico-sdk.lastModifiedDate;
-                year = builtins.substring 0 4 date;
-                month = builtins.substring 4 2 date;
-                day = builtins.substring 6 2 date;
-                iso8601 = "${year}-${month}-${day}";
-              in
-                iso8601;
+            version = makeIso8601 pico-sdk.lastModifiedDate;
 
             src = pico-sdk;
 
@@ -47,6 +56,15 @@
             export PICO_SDK_PATH=@out@/share/pico-sdk
             '';
           };
+
+          openocd = with pkgs; pkgs.openocd.overrideAttrs (oldAttrs: {
+            pname = "openocd-rp2040";
+            version = makeIso8601 openocd.lastModifiedDate;
+
+            src = openocd;
+
+            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ autoreconfHook269 ];
+          });
         };
       }
     );
